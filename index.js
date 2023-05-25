810c7e2839ca364522bef67a537e62e46e56a8e9
jeedeekay <gavinkinder@gmail.com>
2023-05-24 20:43:56 -0700

    Working Login

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const dbConfig = require('./MongoDB');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'ihb2345918236h9786r1d79824jyr78yr8749dr6978236897';
 
-app.use(cors());
+app.use(cors({credentials: true, origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 const url = dbConfig.mongoUrl;
 const connect = mongoose.connect(url);
@@ -26,7 +30,36 @@ app.post('/register', async (req, res) => {
     } catch(e) {
         res.status(400).json(e);
     }
-    
+});
+
+app.post('/login', async (req, res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        jwt.sign({username, id:userDoc._id}, secret, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json({
+                id:userDoc._id,
+                username
+            });
+        });
+    } else {
+        res.status(400).json('WRONG CREDENTIALS');
+    }
+});
+
+app.get('/profile', (req, res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    })
+    res.json(req.cookies);
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
 });
 
 connect.then(() => console.log('Connected correctly to server'), 
@@ -36,4 +69,4 @@ connect.then(() => console.log('Connected correctly to server'),
 app.listen(4000);
 
 // https://www.youtube.com/watch?v=xKs2IZZya7c
-// Paused at 1:09:05, about to set up login script
+// Paused at 1:41:05, about to set up Create New Post functionality
